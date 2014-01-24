ea90a308672614ad05353e313475bfd69e0dd019
Hiroyuki Miyazaki <heromiya@gumaster>
2014-01-24 18:47:10 +0900

    Committer: Hiroyuki Miyazaki <heromiya@gumaster>

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+require_once 'MDB2.php';
+$DBHOST="localhost";
+$WEBHOST="guam.csis.u-tokyo.ac.jp";
+$db = MDB2::connect('pgsql://heromiya@'.$DBHOST.'/adbgeomapping?charset=utf8');
+if(PEAR::isError($db)) {
+    print('There is an error with connection to the database. Please contact with administrator.');
+}
+$proj1=NULL;
+$pid=NULL;
+if(isset($_GET['pid'])) $pid=$_GET['pid'];
+
+if(isset($_GET['proj1'])) $proj1=$_GET['proj1'];
+if($proj1==NULL) $proj1 = "TMS";
+
+$stm = $db->prepare("SELECT ST_XMax(ST_Collect(the_geom))
+                    , ST_XMin(ST_Collect(the_geom))
+                    , ST_YMax(ST_Collect(the_geom))
+                    , ST_Ymin(ST_Collect(the_geom))
+                     FROM allworldbank_ibrdida
+                     WHERE project_id = ?;"
+		    ,array('text')
+		    ,array('float','float','float','float')
+		    );
+$result = $stm->execute($pid);
+
+while ($row = $result->fetchRow()) {
+$lonmax=$row[0];
+$lonmin=$row[1];
+$latmax=$row[2];
+$latmin=$row[3];
+
+}
+
+
+?>
+<html>
+  <head>
+    <script type="text/javascript" src="../OpenLayers-2.12/lib/OpenLayers.js"></script> 
+    <script type="text/javascript" src="../OpenLayers-2.12/lib/deprecated.js"></script>
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="proj4js/lib/proj4js-compressed.js"></script>
+    <script type="text/javascript" src="maplayers.js"></script>
+    <script type="text/javascript" src="openlayers.style.js"></script>
+    <script type="text/javascript" src="map.js"></script>
+    <title>ADB GeoMapping - Validation</title>
+  </head>
+  <body onload="init(<?php printf('%lf,%lf,%lf,%lf,\'%s\'',$lonmin,$latmin,$lonmax,$latmax,$pid)?>)">
+    <table width="100%" height="100%" cellpadding="0" cellspacing="0" >
+      <tr height="20px">
+	<td align="center" class="item"><div id="nodelist"></div></td>
+	<td rowspan="2" width="250px">
+	  <table border="1" width="100%">
+	    <tr><td><div id="project_id">Project ID:</div></td></tr>
+	    <tr><td><div id="project_title">Project Title:</div></td></tr>
+	    <tr><td><div id="country">Country:</div></td></tr>
+	    <tr><td><div id="adm1">Adm1:</div></td></tr>
+	    <tr><td><div id="adm2">Adm2:</div></td></tr>
+	    <tr><td><div id="results">Results:</div></td></tr>
+	    <form name="validation">
+	      <tr><td>The location is <select name="correct" onChange="updateAttributes(attribute);">
+		    <option value=""></option>
+		    <option value="correct">correct</option>
+		    <option value="incorrect">NOT correct</option>
+	      </select></td></tr>
+	      <tr><td>If the location is not correct, please advise correct location by map or text.<textarea name="correction_text" rows="4" cols="40">Please input here.</textarea></td></tr>
+	    </form>
+	  </table>
+	</td>
+      </tr>
+      <tr height="100%">
+	<td id="col1">
+	  <div id="map1" style="height: 100%; background-color: #808080" unselectable = "on" user-select: none;></div>
+	</td>
+      </tr>
+    </table>
+  </body>
+</html>
--- map.js
+++ map.js
@@ -1,13 +1,112 @@
- var map1Nav = new OpenLayers.Control.Navigation();
+var selectControl, selectedFeature, map1,attribute;
+function showMsg(szMessage) {
+    document.getElementById("nodelist").innerHTML = szMessage;
+    setTimeout("document.getElementById('nodelist').innerHTML = ''",3000);
+}
 
-function init(lonmin,latmin,lonmax,latmax) {
+function showSuccessMsg(){
+    showMsg("<img src='Save-icon.png'> Saved.");
+    //alert('Saved');
+    //var nowdate = new Date();
+    //document.getElementById("lastSave").innerHTML = "Last save:" + (Number(nowdate.getMonth())+1) + "/" + nowdate.getDate() + " " + nowdate.getHours() + ":" + nowdate.getMinutes();
+};
+function showFailureMsg(){alert('Problems in data transaction occured. Please tell this to the system administrator.')};
+var saveStrategy = new OpenLayers.Strategy.Save();
+saveStrategy.events.register("success", '', showSuccessMsg);
+saveStrategy.events.register("failure", '', showFailureMsg);
+saveStrategy.auto = true;
+
+function onPopupClose(evt) {
+    selectControl.unselect(selectedFeature);
+}
+/*
+function onFeatureSelect(feature) {
+    selectedFeature = feature;
+    popup = new OpenLayers.Popup.FramedCloud(
+	"chicken"
+	,feature.geometry.getBounds().getCenterLonLat()
+	,null
+	,"<div style='font-size:.8em'>Project ID: " + feature.attributes.project_id
+	    + "<br>Project title: " + feature.attributes.project_title
+	    + "<br>Country: " + feature.attributes.country
+	    + "<br>Adm1: " + feature.attributes.adm1
+	    + "<br>Adm2: " + feature.attributes.adm2
+	    +"</div>"
+	,null
+	,true
+	,onPopupClose
+    );
+    feature.popup = popup;
+    map1.addPopup(popup);
+}
+*/
+
+function attributeStr(feature){
+    str = '<td id="vi" class="item">Class:'
+    if(feature.attributes.vi == 'urban'){
+	str += '<select size="1" name="vi_input" onChange="updateAttributes(attribute);"><option value="urban" selected>urban</option><option value="unknown">unknown</option></select></td>';
+    }else if(feature.attributes.vi == 'unknown') {
+	str += '<select size="1" name="vi_input" onChange="updateAttributes(attribute);"><option value="urban">urban</option><option value="unknown" selected>unknown</option></select></td>';
+    }else {
+	str += '<select size="1" name="vi_input" onChange="updateAttributes(attribute);"><option value="urban">urban</option><option value="unknown">unknown</option><option value="undefined" selected>undefined</option></select></td>';
+    }
+    document.getElementById("vi").innerHTML = str;
+    
+    str = '<td id="note" class="item">Note:'
+    if(feature.attributes.note == null){
+	str += '<input type="text" name="note_input" onChange="updateAttributes(attribute);" value="">';
+    }else{
+	str += '<input type="text" name="note_input" onChange="updateAttributes(attribute);" value="' + feature.attributes.note + '">';
+    }
+    str += '</td>'
+    document.getElementById("note").innerHTML = str;
+}
+
+
+function onFeatureSelect(feature) {
+    selectedFeature = feature;
+    document.getElementById("project_id").innerHTML = "Project ID: " + feature.attributes.project_id;
+    document.getElementById("project_title").innerHTML = "Project Title: " + feature.attributes.project_title;
+    document.getElementById("country").innerHTML = "Country: " + feature.attributes.country;
+    document.getElementById("adm1").innerHTML = "Adm1: " + feature.attributes.adm1;
+    document.getElementById("adm2").innerHTML = "Adm2: " + feature.attributes.adm2;
+    document.getElementById("results").innerHTML = "Results: " + feature.attributes.results;
+
+}
+
+function onFeatureUnselect(feature) {
+    document.getElementById("project_id").innerHTML = "Project ID: ";
+    document.getElementById("project_title").innerHTML = "Project Title: ";
+    document.getElementById("country").innerHTML = "Country: ";
+    document.getElementById("adm1").innerHTML = "Adm1: ";
+    document.getElementById("adm2").innerHTML = "Adm2: ";
+    document.getElementById("results").innerHTML = "Results: ";
+//    map1.removePopup(feature.popup);
+//    feature.popup.destroy();
+//    feature.popup = null;
+}   
+
+var poi_style = new OpenLayers.StyleMap(
+    OpenLayers.Util.applyDefaults(
+	{ pointRadius: 7,
+	  fillOpacity: 1,
+	  externalGraphic: "map_pin.png"
+	},
+	OpenLayers.Feature.Vector.style["default"]));
+
+function updateAttributes(attribute){
+    selectedFeature.attributes.validation = document.validation.correct.value;
+    selectedFeature.state = OpenLayers.State.UPDATE;
+    saveStrategy.save();
+};
+
+function init(lonmin,latmin,lonmax,latmax,pid) {
     extlonmin = lonmin;
     extlatmin = latmin;
     extlonmax = lonmax;
     extlatmax = latmax;
     var map1extent = new OpenLayers.Bounds(lonmin,latmin,lonmax,latmax).transform(WGS84,TMS);
     var mapextent_wgs84 = new OpenLayers.Bounds(lonmin,latmin,lonmax,latmax);
-
     var map1Opts = {
 	displayProjection: TMS
 	,projection: TMS
@@ -15,26 +114,26 @@ function init(lonmin,latmin,lonmax,latmax) {
 	,numZoomLevels: 20
 	,maxExtent: map1extent
 	,controls:[
-	    map1Nav,
-	    new OpenLayers.Control.PanZoom()
+	    new OpenLayers.Control.Navigation()
+	    ,new OpenLayers.Control.PanZoom()
     	    ,new OpenLayers.Control.MousePosition()
 	    ,new OpenLayers.Control.KeyboardDefaults() 
 	    ,new OpenLayers.Control.Scale()
 	    ,new OpenLayers.Control.OverviewMap()
+	    ,new OpenLayers.Control.LayerSwitcher()
 	]
 	,eventListeners: {
 	}
     }
-
     map1 = new OpenLayers.Map('map1',map1Opts);
-
     var wfs_layer = new OpenLayers.Layer.Vector(
-	"Editable Features"
+	"Project Location"
 	,{
 	    strategies: [
 		new OpenLayers.Strategy.BBOX()
+		,saveStrategy
 	    ]
-	    ,styleMap: new OpenLayers.StyleMap(style)
+	    //,styleMap: new OpenLayers.StyleMap(poi_style)
 	    ,protocol: new OpenLayers.Protocol.WFS(
 		{
 		    version: "1.1.0",
@@ -47,14 +146,51 @@ function init(lonmin,latmin,lonmax,latmax) {
 		    schema: "http://guam.csis.u-tokyo.ac.jp:28080/geoserver/wfs/DescribeFeatureType?version=1.1.0&typename=cite:allworldbank_ibrdida"
 		})
 	    ,eventListeners: {
+		"loadstart": function(){
+		    document.getElementById("nodelist").innerHTML = "<img src='loader.gif'> Loading ...";
+		},
+		"loadend": function(){
+		    document.getElementById("nodelist").innerHTML = "<img src='Check-icon.png'> Completed.";
+		    setTimeout("document.getElementById('nodelist').innerHTML = ''",5000);
+		}
 	    }
+            ,filter: new OpenLayers.Filter.Logical(
+		{type: OpenLayers.Filter.Logical.AND,
+		 filters: [
+		     new OpenLayers.Filter.Comparison(
+			 {type: OpenLayers.Filter.Comparison.EQUAL_TO,
+			  property: "project_id",
+			  value: pid
+			 })
+		 ]
+		})
 	}
     );
+
+    wfs_layer.events.on({
+        "featuremodified": function(event){
+	    event.feature.state = OpenLayers.State.UPDATE;
+	    saveStrategy.save();
+	},
+	"beforefeatureadded": function(event){
+	    feature = event.feature;
+	    if (feature.attributes.validation == null)
+		feature.attributes.validation = 'NULL';
+	},
+    });
     
-    map1.addLayers([google_maps,wfs_layer]);
+    map1.addLayers([google_maps,google_satellite,google_hybrid,osm,bing_road,bing_aerial,bing_hybrid,wfs_layer]);
     map1.zoomToExtent(map1extent);
 
-    var navigate = new OpenLayers.Control.Navigation(
+    selectControl = new OpenLayers.Control.SelectFeature(
+	wfs_layer
+	,{ onSelect: onFeatureSelect
+	  ,onUnselect: onFeatureUnselect});
+    map1.addControl(selectControl);
+    selectControl.activate();
+    saveStrategy.activate();
+
+/*    var navigate = new OpenLayers.Control.Navigation(
 	{
 	    title: "Pan Map"
 	    ,eventListeners: {
@@ -68,11 +204,5 @@ function init(lonmin,latmin,lonmax,latmax) {
 	    ,type: OpenLayers.Control.TYPE_TOGGLE
 	}
     );
-
-    //panel.addControls([navigate]);
-    //panel.defaultControl = navigate;
-    //map1.addControl(panel);
-    //map1.fractionalZoom =true;
-
-   
+  */ 
 };
